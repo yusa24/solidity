b19204563afc0ec193e394a8536c894f2f3978d2
Christian Parpart <christian@parpart.family>
2020-07-22 16:22:21 +0200

    Removes AppVeyor CI

--- scripts/release.bat
+++ scripts/release.bat
@@ -29,9 +29,11 @@ REM ---------------------------------------------------------------------------
 set CONFIGURATION=%1
 set VERSION=%2
 
+REM Why is this looking for x86 folder and not x64 folder?
+
 set "DLLS=MSVC_DLLS_NOT_FOUND"
-FOR /d %%d IN ("C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Redist\MSVC\*"
-    "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\*") DO set "DLLS=%%d\x86\Microsoft.VC141.CRT\msvc*.dll"
+FOR /d %%d IN ("C:\Program Files (x86)\Microsoft Visual Studio\%VERSION%\BuildTools\VC\Redist\MSVC\*"
+    "C:\Program Files (x86)\Microsoft Visual Studio\%VERSION%\Community\VC\Redist\MSVC\*") DO set "DLLS=%%d\x86\Microsoft.VC142.CRT\msvc*.dll"
 
 7z a solidity-windows.zip ^
     .\build\solc\%CONFIGURATION%\solc.exe .\build\test\%CONFIGURATION%\soltest.exe ^

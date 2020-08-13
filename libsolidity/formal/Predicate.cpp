53be9be5d50dd15871211615846310c9e171c15c
Leonardo Alt <leo@ethereum.org>
2020-08-13 14:00:33 +0200

    Refactor predicates

---
+++ libsolidity/formal/Predicate.cpp
@@ -0,0 +1,88 @@
+/*
+	This file is part of solidity.
+
+	solidity is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	solidity is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
+*/
+// SPDX-License-Identifier: GPL-3.0
+
+#include <libsolidity/formal/Predicate.h>
+
+#include <libsolidity/ast/AST.h>
+
+using namespace std;
+using namespace solidity;
+using namespace solidity::smtutil;
+using namespace solidity::frontend;
+using namespace solidity::frontend::smt;
+
+map<string, Predicate> Predicate::m_predicates;
+
+Predicate const* Predicate::create(
+	SortPointer _sort,
+	string _name,
+	EncodingContext& _context,
+	ASTNode const* _node
+)
+{
+	Predicate pred{_sort, _name, _context, _node};
+	string functorName = pred.functor().name;
+	solAssert(!m_predicates.count(functorName), "");
+	m_predicates.emplace(functorName, move(pred));
+	return &m_predicates.at(functorName);
+}
+
+Predicate::Predicate(
+	SortPointer _sort,
+	string _name,
+	EncodingContext& _context,
+	ASTNode const* _node
+):
+	m_predicate(_sort, move(_name), _context),
+	m_node(_node)
+{
+}
+
+Predicate const* Predicate::predicate(std::string const& _name)
+{
+	return &m_predicates.at(_name);
+}
+
+void Predicate::reset()
+{
+	m_predicates.clear();
+}
+
+smtutil::Expression Predicate::operator()(std::vector<smtutil::Expression> const& _args) const
+{
+	return m_predicate(_args);
+}
+
+smtutil::Expression Predicate::functor() const
+{
+	return m_predicate.currentFunctionValue();
+}
+
+smtutil::Expression Predicate::functor(unsigned _idx) const
+{
+	return m_predicate.functionValueAtIndex(_idx);
+}
+
+void Predicate::newFunctor()
+{
+	m_predicate.increaseIndex();
+}
+
+ASTNode const* Predicate::programNode() const {
+	return m_node;
+}
---
+++ libsolidity/formal/Predicate.h
@@ -0,0 +1,82 @@
+/*
+	This file is part of solidity.
+
+	solidity is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	solidity is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
+*/
+// SPDX-License-Identifier: GPL-3.0
+
+#pragma once
+
+#include <libsolidity/formal/SymbolicVariables.h>
+#include <libsolidity/formal/SymbolicVariables.h>
+
+#include <libsmtutil/Sorts.h>
+
+#include <map>
+#include <vector>
+
+namespace solidity::frontend
+{
+
+/**
+ * Represents a predicate used by the CHC engine.
+ */
+class Predicate
+{
+public:
+	static Predicate const* create(
+		smtutil::SortPointer _sort,
+		std::string _name,
+		smt::EncodingContext& _context,
+		ASTNode const* _node = nullptr
+	);
+
+	static Predicate const* predicate(std::string const& _name);
+
+	static void reset();
+
+	/// @returns a function application of the predicate over _args.
+	smtutil::Expression operator()(std::vector<smtutil::Expression> const& _args) const;
+
+	/// @returns the function declaration of the predicate.
+	smtutil::Expression functor() const;
+	/// @returns the function declaration of the predicate with index _idx.
+	smtutil::Expression functor(unsigned _idx) const;
+	/// Increases the index of the function declaration of the predicate.
+	void newFunctor();
+
+	/// @returns the program node this predicate represents.
+	ASTNode const* programNode() const;
+
+private:
+	Predicate(
+		smtutil::SortPointer _sort,
+		std::string _name,
+		smt::EncodingContext& _context,
+		ASTNode const* _node = nullptr
+	);
+
+	/// The actual SMT expression.
+	smt::SymbolicFunctionVariable m_predicate;
+
+	/// The ASTNode that this predicate represents.
+	/// nullptr if this predicate is not associated with a specific program AST node.
+	ASTNode const* m_node = nullptr;
+
+	/// Maps the name of the predicate to the actual Predicate.
+	/// Used in counterexample generation.
+	static std::map<std::string, Predicate> m_predicates;
+};
+
+}
